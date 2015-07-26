98c5d4a4ffb15c6ede533ecf9e4c62d38d246915
Thiago Pagonha <thi.pag@gmail.com>
2015-07-26 00:58:37 -0300

    Reconfiguring project

--- restserver/wsgi.py
+++ restserver/wsgi.py
@@ -12,6 +12,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 application = DjangoWhiteNoise(application)
