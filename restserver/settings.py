0b159a8e4c0098f6a75d4415007300a2b1a3bf33
Thiago Pagonha <thi.pag@gmail.com>
2015-07-26 18:02:44 -0300

    Permissions managed by django's default user permissions

--- restserver/settings.py
+++ restserver/settings.py
@@ -129,7 +129,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.DjangoModelPermissions',
     ],
     'PAGE_SIZE' : 10 
 }
