ae0f69e07f6022c568db9c0413e2a63a000be911
Thiago Pagonha <thi.pag@gmail.com>
2015-07-26 04:22:35 -0300

    Fixing more import mistakes

--- restserver/views.py
+++ restserver/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from restserver.serializers import UserSerializer, GroupSerializer
+from restserver.serializers import *
 from restserver.models import *
 
 class UserViewSet(viewsets.ModelViewSet):
