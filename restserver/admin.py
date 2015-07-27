b0cc8290358cf921bacc8490341e8863ea548c48
Thiago Pagonha <thi.pag@gmail.com>
2015-07-27 01:59:16 -0300

    Adding cosmetics

--- restserver/admin.py
+++ restserver/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from models import *
 
-admin.site.register(Country)
-admin.site.register(City)
-admin.site.register(Course)
-admin.site.register(School)
+# Base Model to show real image of Icons intead of urls in forms
+class ShowIconBaseAdminModel(admin.ModelAdmin):
+    readonly_fields = ('icon_display',)
+    
+    def icon_display(self, instance):
+        return u'<img src="%s" />' % instance.icon
+
+    icon_display.short_description = 'Icon Display Image'
+    icon_display.allow_tags = True
+
+admin.site.register(Country,ShowIconBaseAdminModel)
+admin.site.register(City,ShowIconBaseAdminModel)
+admin.site.register(Course,ShowIconBaseAdminModel)
+admin.site.register(School,ShowIconBaseAdminModel)
 admin.site.register(SchoolCourseValue)
 
     
