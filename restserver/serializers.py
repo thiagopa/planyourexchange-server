4c01b2054ccb58d006dec80b871a9bf2c23e33e3
Thiago Pagonha <thi.pag@gmail.com>
2015-08-01 18:19:13 -0300

    Changing hyperlinked structure to id's and adding method for city lookup according to the given parent country

--- restserver/serializers.py
+++ restserver/serializers.py
@@ -1,24 +1,24 @@
 from restserver.models import *
 from rest_framework import serializers
 
-class CountrySerializer(serializers.HyperlinkedModelSerializer):
+class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
 
-class CitySerializer(serializers.HyperlinkedModelSerializer):
+class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
 
-class CourseSerializer(serializers.HyperlinkedModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
 
-class SchoolSerializer(serializers.HyperlinkedModelSerializer):
+class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
 
 # Custom Serializer for weekly price of courses by school
-class SchoolCourseValueSerializer(serializers.HyperlinkedModelSerializer):
+class SchoolCourseValueSerializer(serializers.ModelSerializer):
 
     week_price = serializers.ReadOnlyField(source='week_price.amount')
     
--- restserver/views.py
+++ restserver/views.py
@@ -1,10 +1,19 @@
 from rest_framework import viewsets
 from restserver.serializers import *
 from restserver.models import *
+from rest_framework.response import Response
+from rest_framework.decorators import detail_route, list_route
 
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    
+    # Extra method for loading cities according to the parent country
+    @detail_route()
+    def cities(self,request,pk=None):
+        queryset = City.objects.filter(country=self.get_object())
+        serializer = CitySerializer(queryset, many=True)
+        return Response(serializer.data)
     
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
