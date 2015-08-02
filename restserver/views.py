ae60890afdb06cbc698dcfc4370973c6c252c7c1
Thiago Pagonha <thi.pag@gmail.com>
2015-08-02 04:43:33 -0300

    Retrieve courses from given city

--- restserver/views.py
+++ restserver/views.py
@@ -19,6 +19,15 @@ class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
     
+    # Load courses according to the city
+    @detail_route()
+    def courses(self,request,pk=None):
+        queryset = SchoolCourseValue.objects.filter(school__city=self.get_object())
+        # Transfer all courses from relationship to serializer
+        serializer = CourseSerializer([scv.course for scv in queryset], many=True)
+        return Response(serializer.data)
+        
+    
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
